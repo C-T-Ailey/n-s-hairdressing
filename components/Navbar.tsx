5d53b3f4d41229a38dd0151fd742df4f43fcb886
Chris Ailey <chris.ailey90@gmail.com>
2023-10-26 19:07:51 +0100

    added new favicon.ico; implemented React Icons node package for social media and other decorative icons; implemented Room-Medium font; styling changes to Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,35 @@
 import Link from "next/link";
+import { BsFacebook, BsInstagram, BsDiamondFill } from "react-icons/bs"
 
 export default function Navbar(){
     return (
-      <div className="fixed w-full h-12 bg-white flex justify-evenly items-center">
+      <div className="fixed w-full h-12 bg-white flex justify-between items-center px-6 text-xl">
         <h1>Bookings: 555-1234</h1>
-        <ul className=" list-none w-1/3 flex justify-evenly">
+        <ul className="list-none w-1/2 flex justify-evenly items-center">
             <li>
                 <Link href="/">Home</Link>
             </li>
+            <li><BsDiamondFill size={10} className="mt-1" /></li>
             <li>
                 <Link href="/services">Services & Pricing</Link>
             </li>
+            <li><BsDiamondFill size={10} className="mt-1" /></li>
             <li>
                 <Link href="/gallery">Gallery</Link>
             </li>
+            <li><BsDiamondFill size={10} className="mt-1" /></li>
             <li>
                 <Link href="/about">About Me</Link>
             </li>
+            <li><BsDiamondFill size={10} className="mt-1" /></li>
             <li>
                 <Link href="/contact">Contact</Link>
             </li>
         </ul>
-        <h1>Socials will go here</h1>
+        <div className="flex row w-20 justify-between">
+            <BsFacebook size={24}/>
+            <BsInstagram size={24}/>
+        </div>
       </div>
     )
 }
