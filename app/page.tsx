c506ce4749645bb3e8a0c78d65b0b20ebcf135d8
Chris Ailey <chris.ailey90@gmail.com>
2024-01-23 14:42:16 +0000

    removed eslint rule disabling no-escaped-characters, fixed issue in app/page.tsx causing the no-escaped-characters error

--- app/page.tsx
+++ app/page.tsx
@@ -15,11 +15,11 @@ export default function Home() {
           />
       </div>
       <div className='h-[30vh] flex flex-col justify-center'>
-        <div className='h-[20vh] flex flex-col justify-around text-center text-2xl text-[#606060]'>
-          <p className='animate-fade animate-once animate-duration-1000 animate-ease-in'>Hi! I'm Nicki.</p>
+        <div className='h-[20vh] flex flex-col justify-around text-center text-2xl text-[#606060]' style={{userSelect:"none"}}>
+          <p className='animate-fade animate-once animate-duration-1000 animate-ease-in'>Hi! I&apos;m Nicki.</p>
           <p className='animate-fade animate-once animate-duration-1000 animate-delay-[1500ms] animate-ease-in'>As an independent stylist with over 25 years of experience in the salon,</p>
           <p className='animate-fade animate-once animate-duration-1000 animate-delay-[3000ms] animate-ease-in'>I know how important that personal touch is when it comes to your haircare.</p>
-          <p className='animate-fade animate-once animate-duration-1000 animate-delay-[4500ms] animate-ease-in'>That's why, whoever you are and whatever you need,</p>
+          <p className='animate-fade animate-once animate-duration-1000 animate-delay-[4500ms] animate-ease-in'>That&apos;s why, whoever you are and whatever you need,</p>
           <p className='animate-fade animate-once animate-duration-1000 animate-delay-[6000ms] animate-ease-in'>my priority will always be to make sure you get the attention and service you deserve.</p>
         </div>
       </div>
